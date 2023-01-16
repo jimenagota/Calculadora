7ea4f4b5f50dac7266ea1f35b38fcf79cb53dd0a
Jimena Tinajero <jimenatinajero@Sergios-MacBook-Air.local>
2023-01-15 23:19:08 -0600

    Menú principal de la calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,1 +1,25 @@
 #Calculadora
+print("Iniciando calculadora...")
+
+#Variables
+acciones = ["0: Menú principal", "1: Suma", "2: Resta", "3: Multiplicación", "4: División", "5: Raíz n", "6: Exponente n", "7: Seno", "8: Salir"]
+
+def iniciarCalculadora():
+    isOn = True
+    while isOn:
+        print("Seleccione la opción deseada:")
+        for x in acciones:
+            print(x)
+        accion = int(input())
+        match accion:
+            case 0:
+                print("Menú principal")
+            case 1:
+                print("Suma:")
+            case 8:
+                print("Hasta pronto.")
+                isOn = False
+        
+
+#Inicio de la aplicación
+iniciarCalculadora()
